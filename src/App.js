02f7dfcfde02bb93971eb6a84db9569986b1cad6
abohashemelrefaay <60824021+abohashemelrefaay@users.noreply.github.com>
2022-12-25 13:12:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/navbar";
+import Manage from "./components/manageplatform/manage";
+import Services from "./components/services/services";
+import Testimonial from "./components/testimonials/testimonial";
+import Price from "./components/price/price";
+import Business from "./components/business/business";
 import Foter from "./components/footer/foter";
 import Allcomponents from "./components/allcomponents/allcomponents";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Allcomponents  /> 
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Allcomponents />} />
+          <Route exact path="/About" element={<Business />} />
+          <Route exact path="/Services" element={<Services />} />
+          <Route exact path="/Pricing" element={<Price />} />
+          <Route exact path="/FAQ" element={<Testimonial />} />
+          <Route exact path="/Contact" element={<Manage />} />
+        </Routes>
+      </Router>
       <Foter />
     </div>
   );
 }
 
-export default App;
+export default App; App;
